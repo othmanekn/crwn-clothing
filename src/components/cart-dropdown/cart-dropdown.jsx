5d0b5f65fe337d96fc525c3e1cd5f135de1a3f17
othmanekn <othmane.elkinay2@gmail.com>
2020-07-05 19:09:19 +0100

    created cart-item womponnet and connected our cart dropdown with our cartReducer

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./cart-dropdown.scss";
 import CustomButton from "../custom-button/custon-button";
+import CartItem from "../cart-item/cart-item";
+import { connect } from "react-redux";
 
-const CartDropwdown = () => (
+const CartDropwdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.item.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>Go to checkout</CustomButton>
   </div>
 );
 
-export default CartDropwdown;
+const mapStateToProps = ({ cart }) => ({
+  cartItems: cart.cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropwdown);
